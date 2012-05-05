29bbc55dd3f2d294bfba03c993273480e8a75ad4
Azrathud <azrathud@gmail.com>
2012-05-05 14:48:25 -0600

    Initilizing repository.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+<title>Welcome</title>
+
+<head>
+
+<style type="text/css" media="all">@import "master.css";</style>
+
+</head>
+
+<body>
+	<div id="navigation">
+		<a href="index.php"><img id="hometab" src="hometab.JPG" alt="Home" /></a>
+		<a href="index.php"><img id="hometab" src="hometab.JPG" alt="Home" /></a>
+		<a href="index.php"><img id="hometab" src="hometab.JPG" alt="Home" /></a>
+		<a href="index.php"><img id="hometab" src="hometab.JPG" alt="Home" /></a>
+	</div>	
+
+		<div id="header">
+			<div id="logo">
+			<h4>Logo image goes here.</h4>
+			</div>
+		
+			<div id="firstlogo">
+			<a href="http://www.usfirst.org" target="_blank"><img id="firstlogoimg" src="firstlogo.png" alt ="FIRST Robotics Logo"  /></a>
+			</div>
+		</div>
+
+	<div id="container">
+
+		
+	</div>
+</body>
+
+</html>
+
