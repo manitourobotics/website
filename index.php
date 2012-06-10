262dea512d252f252e01a664679d2b643b873432
Wesley <wesley@wesley-laptop.(none)>
2012-06-10 00:40:40 -0600

    New Files

--- index.php
+++ index.php
@@ -1,23 +1,43 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<link rel="shortcut icon" href="./favicon.ico">
-	<title>2945</title>
-</head>
-<body>
-    <div id="nav">
-    	<li class="nav">Home</li>
-    	<li class="nav">Our Team</li>
-    	<li class="nav">Events</li>
-    	<li class="nav">Sponsors</li>
-    	<li class="nav">Resources</li>
-    	<li class="nav">Calendar</li>
-    	<li class="nav">Contact</li>
-    	<li class="nav"></li>
-    </div>
-    <div id="outside-links">
-    </div>
-	
-</body>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>First Team 2945 - Welcome</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+    	<div class="page">
+     	   <div id="header">
+        		<div id="header_name">
+        			<h1>MANITOU ROBOTICS 2945</h1>
+        		</div>
+        		<div id="header_navagation">
+        			<ul id="navagation">
+  						<li><a href="">Home</a></li>
+  						<li><a href="">About Us</a>
+  							<div id="dropdown">
+   								<ul>
+    								<li><a href="">The Team</a></li>
+    								<li><a href="">History</a></li>
+    								<li><a href="">Vision</a></li>
+   						 		</ul>
+   						 	</div>
+   						</li>
+  						<li><a href="">News/Updates</a></li>
+  						<li><a href="">Contact</a></li>
+					</ul>
+        		</div>
+       		</div>
+       		<div id="sidebar">
+       			<h1>Sidebar</h1>
+       			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit, leo id luctus imperdiet, diam arcu luctus ante, sit amet lacinia mi leo ut orci. Phasellus imperdiet nisi vel erat mollis condimentum. Curabitur eu erat diam, ac consequat lacus.</p>
+       		</div>
+       		<div id="content">
+       			<h2 id="content_import">Important</h2>
+       			<div id="line"><!--Just a line--></div>
+       			
+       		</div>
+        </div>
+    </body>
 </html>
